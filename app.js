05522ea448c4d8c2e2f130155af07d484013f3d0
Aipakka <victor@fadewi.net>
2023-02-09 10:49:40 +0100

    haha

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("tits")
+const express=require("express");
+const bcrypt=require("bcrypt");
+const path=require("path");
+const { response }=require("express");
+const db=require("better-sqlite3")("ChatWebpageDB.db");
+const session = require("express-session");
+const app=express();
+app.use(express.urlencoded({extended: true}));
+const publicPath=path.join(__dirname, "/Public");
+app.use(express.static(publicPath));

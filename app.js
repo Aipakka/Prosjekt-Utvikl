95c1b475b31865e2a2203f0a618c6be04cf2df0f
Aipakka <victor@fadewi.net>
2023-02-14 08:42:52 +0100

    h

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ const app=express();
 app.use(express.urlencoded({extended: true}));
 const publicPath=path.join(__dirname, "/Public");
 app.use(express.static(publicPath));
+
+
+
+
+
+
+
+
+app.listen(port, bgg)
+function bgg(){
+    console.log("i am listenin g to your mother aslo called: " + port)
+}

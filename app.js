36bab6bc680967ce8720127b00dbfb1e4576a650
Aipakka <victor@fadewi.net>
2023-02-17 12:14:11 +0100

    signup done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { response }=require("express");
 const db=require("better-sqlite3")("prosjektdb.sdb");
 const session = require("express-session");
 const { request } = require("http");
+const { Script } = require("vm");
 const app=express();
 const port =3000;
 app.use(express.urlencoded({extended: true}));
@@ -12,13 +13,28 @@ const publicPath=path.join(__dirname, "/Public");
 app.use(express.static(publicPath));
 
 app.post("/sendinn",  async(request, response) =>{
-    //const svar = request.body;
+    const data = request.body;
     let hash = await bcrypt.hash(data.password, 10)
-    db.prepare("INSERT INTO user (username, password ) VALUES (?, ?)").run(data.username, hash)
+    if (!db.prepare("SELECT username FROM user WHERE username=?").get(data.username)){
+        db.prepare("INSERT INTO user (username, password ) VALUES (?, ?)").run(data.username, hash)
     response.redirect("/login.html")
-
+    }else{
+    response.redirect("/signuperror.html")
+    }
 })
 
+app.post("/login",  async(request, response) =>{
+    const data = request.body;
+    let hash = await bcrypt.hash(data.password, 10)
+    db.prepare("SELECT username, password FROM user WHERE username=").run(data.username, hash)
+    response.redirect("/login.html")
+    
+
+})
+app.get("/", (request, response) =>{
+    response.redirect("/index.html")
+}
+)
 app.listen(port, bgg)
 function bgg(){
     console.log("i am listenin g to your mother aslo called: " + port)

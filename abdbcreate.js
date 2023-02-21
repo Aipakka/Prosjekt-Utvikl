58db5ecbc1cc2582a5c2fc4afe4e39d2c9aa33b6
Aipakka <victor@fadewi.net>
2023-02-21 13:45:28 +0100

    endret database

--- abdbcreate.js
+++ abdbcreate.js
@@ -1,6 +1,5 @@
 const db =require("better-sqlite3")("")
-db.exec(`
--- Creator:       MySQL Workbench 8.0.27/ExportSQLite Plugin 0.1.0
+db.exec(`-- Creator:       MySQL Workbench 8.0.27/ExportSQLite Plugin 0.1.0
 -- Author:        Victor Wiegand
 -- Caption:       New Model
 -- Project:       Name of the project
@@ -35,18 +34,5 @@ CREATE TABLE "mydb"."shoppinCart"(
 );
 CREATE INDEX "mydb"."shoppinCart.fk_user_has_products_products1_idx" ON "shoppinCart" ("products_idproducts");
 CREATE INDEX "mydb"."shoppinCart.fk_user_has_products_user_idx" ON "shoppinCart" ("user_iduser");
-CREATE TABLE "mydb"."purchaseHistoryxx"(
-  "user_iduser" INTEGER NOT NULL,
-  "products_idproducts" INTEGER NOT NULL,
-  PRIMARY KEY("user_iduser","products_idproducts"),
-  CONSTRAINT "fk_user_has_products_user1"
-    FOREIGN KEY("user_iduser")
-    REFERENCES "user"("iduser"),
-  CONSTRAINT "fk_user_has_products_products2"
-    FOREIGN KEY("products_idproducts")
-    REFERENCES "products"("idproducts")
-);
-CREATE INDEX "mydb"."purchaseHistoryxx.fk_user_has_products_products2_idx" ON "purchaseHistoryxx" ("products_idproducts");
-CREATE INDEX "mydb"."purchaseHistoryxx.fk_user_has_products_user1_idx" ON "purchaseHistoryxx" ("user_iduser");
 COMMIT;
 `)
